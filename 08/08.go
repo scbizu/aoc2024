c9f5af711fbcc28d42d82f0c27bd6a231263d6d3
scnace <scbizu@gmail.com>
2024-12-09 00:47:12 +0800

    08: p2: geo and manhattan distance

--- 08/08.go
+++ 08/08.go
@@ -9,29 +9,12 @@ import (
 	"github.com/magejiCoder/magejiAoc/set"
 )
 
-// line: y = kx + b
 type line struct {
-	k       int
-	b       int
 	origins [2]grid.Vec
 }
 
-func (l line) String() string {
-	return fmt.Sprintf("y = %dx + %d", l.k, l.b)
-}
-
-func (l line) IsPerfect(v grid.Vec) bool {
-	return l.k*v.X+l.b == v.Y && (grid.Distance(l.origins[0], v) == 2*grid.Distance(l.origins[1], v) ||
-		grid.Distance(l.origins[1], v) == 2*grid.Distance(l.origins[0], v))
-}
-
-// 两个向量确定一条直线
 func NewLine(v1, v2 grid.Vec) line {
-	k := (v2.Y - v1.Y) / (v2.X - v1.X)
-	b := v1.Y - k*v1.X
 	return line{
-		k:       k,
-		b:       b,
 		origins: [2]grid.Vec{v1, v2},
 	}
 }
@@ -40,7 +23,7 @@ func p1(ctx context.Context) {
 	txt := input.NewTXTFile("08.txt")
 	g := grid.NewVecMatrix[byte]()
 	cmap := make(map[byte][]grid.Vec)
-	originSet := set.New[grid.Vec]()
+	uniques := set.New[grid.Vec]()
 	txt.ReadByLineEx(ctx, func(i int, line string) error {
 		for j, c := range line {
 			g.Add(grid.Vec{
@@ -52,7 +35,7 @@ func p1(ctx context.Context) {
 					X: j,
 					Y: i,
 				})
-				originSet.Add(grid.Vec{
+				uniques.Add(grid.Vec{
 					X: j,
 					Y: i,
 				})
@@ -66,29 +49,123 @@ func p1(ctx context.Context) {
 	for c, vecs := range cmap {
 		for i := 0; i < len(vecs); i++ {
 			for j := i + 1; j < len(vecs); j++ {
-				lines[c] = append(lines[c], NewLine(vecs[i], vecs[j]))
+				// fmt.Printf("vecs: %v,%v\n", vecs[i], vecs[j])
+				ln := NewLine(vecs[i], vecs[j])
+				// fmt.Printf("current on antennas: %s,line: %s\n", string(c), ln)
+				lines[c] = append(lines[c], ln)
 			}
 		}
 	}
-	antiNodes := set.New[grid.Vec]()
-	for c, ls := range lines {
+	var total int
+	for _, ls := range lines {
 		for _, l := range ls {
-			fmt.Printf("current on antennas: %s,line: %s\n", string(c), l)
-			g.ForEach(func(v grid.Vec, b byte) {
-				if l.IsPerfect(v) {
-					if !originSet.Has(v) {
-						fmt.Printf("found: antiNode: %v\n", v)
-						antiNodes.Add(v)
-					}
-				}
-			})
+			// fmt.Printf("current on antennas: %s,line: %s\n", string(c), l)
+			h, w := l.origins[1].Y-l.origins[0].Y, l.origins[1].X-l.origins[0].X
+			node1 := grid.Vec{
+				X: l.origins[0].X - w,
+				Y: l.origins[0].Y - h,
+			}
+			if _, ok := g.Get(node1); ok && !uniques.Has(node1) {
+				// fmt.Printf("node1: %v\n", node1)
+				total++
+			}
+			node2 := grid.Vec{
+				X: l.origins[1].X + w,
+				Y: l.origins[1].Y + h,
+			}
+			if _, ok := g.Get(node2); ok && !uniques.Has(node2) {
+				// fmt.Printf("node2: %v\n", node2)
+				total++
+			}
 		}
 	}
 
-	fmt.Printf("p1: %d\n", antiNodes.Size())
+	fmt.Printf("p1: %d\n", total)
+}
+
+func p2(ctx context.Context) {
+	txt := input.NewTXTFile("08.txt")
+	g := grid.NewVecMatrix[byte]()
+	cmap := make(map[byte][]grid.Vec)
+	uniques := set.New[grid.Vec]()
+	txt.ReadByLineEx(ctx, func(i int, line string) error {
+		for j, c := range line {
+			g.Add(grid.Vec{
+				X: j,
+				Y: i,
+			}, byte(c))
+			if c != '.' {
+				cmap[byte(c)] = append(cmap[byte(c)], grid.Vec{
+					X: j,
+					Y: i,
+				})
+				uniques.Add(grid.Vec{
+					X: j,
+					Y: i,
+				})
+			}
+		}
+		return nil
+	})
+
+	lines := make(map[byte][]line)
+
+	for c, vecs := range cmap {
+		for i := 0; i < len(vecs); i++ {
+			for j := i + 1; j < len(vecs); j++ {
+				// fmt.Printf("vecs: %v,%v\n", vecs[i], vecs[j])
+				ln := NewLine(vecs[i], vecs[j])
+				// fmt.Printf("current on antennas: %s,line: %s\n", string(c), ln)
+				lines[c] = append(lines[c], ln)
+			}
+		}
+	}
+	for _, ls := range lines {
+		for _, l := range ls {
+			// fmt.Printf("current on antennas: %s,line: %s\n", string(c), l)
+			h, w := l.origins[1].Y-l.origins[0].Y, l.origins[1].X-l.origins[0].X
+			n1 := grid.Vec{
+				X: l.origins[0].X - w,
+				Y: l.origins[0].Y - h,
+			}
+			for {
+				if _, ok := g.Get(n1); !ok {
+					break
+				}
+				if !uniques.Has(n1) {
+					// fmt.Printf("n1: %v\n", n1)
+					uniques.Add(n1)
+				}
+				n1 = grid.Vec{
+					X: n1.X - w,
+					Y: n1.Y - h,
+				}
+			}
+			n2 := grid.Vec{
+				X: l.origins[1].X + w,
+				Y: l.origins[1].Y + h,
+			}
+			for {
+				if _, ok := g.Get(n2); !ok {
+					break
+				}
+				if !uniques.Has(n2) {
+					// fmt.Printf("n2: %v\n", n2)
+					uniques.Add(n2)
+				}
+				n2 = grid.Vec{
+					X: n2.X + w,
+					Y: n2.Y + h,
+				}
+			}
+		}
+	}
+
+	fmt.Printf("p2: %d\n", uniques.Size())
 }
 
 func main() {
 	ctx := context.TODO()
 	p1(ctx)
+	p2(ctx)
 }
