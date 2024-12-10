5084139bfff6f0c4e8a7747654d8d21daadc0cad
scnace <scbizu@gmail.com>
2024-12-11 01:50:20 +0800

    10: maze puzzle

--- 10/10.go
+++ 10/10.go
@@ -16,21 +16,59 @@ type maze struct {
 func (m maze) walk() int {
 	var total int
 	m.g.ForEach(func(v grid.Vec, u uint8) {
-		if u != '9' {
+		if u != '0' {
 			return
 		}
+		v9 := make(map[grid.Vec]struct{})
 		q := queue.NewQueue[grid.Vec]()
 		q.Push(v)
-		fmt.Printf("current: %v\n", v)
 		for q.Len() > 0 {
 			n := q.Pop()
-			if m.g[n] == '0' {
-				total++
+			if m.g[n] == '9' {
+				if _, ok := v9[n]; !ok {
+					v9[n] = struct{}{}
+					total++
+				}
 			}
 			for _, next := range m.g.GetNeighbor(n) {
-				if m.g[next] == m.g[n]-1 {
-					fmt.Printf("next: %v\n", next)
+				if m.g[next] == m.g[n]+1 {
 					q.Push(next)
+				}
+			}
+		}
+	})
+	return total
+}
+
+type traceItem struct {
+	head   grid.Vec
+	trails []grid.Vec
+}
+
+func (m maze) walk2() int {
+	var total int
+	m.g.ForEach(func(v grid.Vec, u uint8) {
+		if u != '0' {
+			return
+		}
+		q := queue.NewQueue[*traceItem]()
+		q.Push(&traceItem{
+			head:   v,
+			trails: []grid.Vec{v},
+		})
+		for q.Len() > 0 {
+			n := q.Pop()
+			if m.g[n.head] == '9' {
+				if len(n.trails) == 10 {
+					total++
+				}
+			}
+			for _, next := range m.g.GetNeighbor(n.head) {
+				if m.g[next] == m.g[n.head]+1 {
+					q.Push(&traceItem{
+						head:   next,
+						trails: append(n.trails, next),
+					})
 				}
 			}
 		}
@@ -54,7 +92,24 @@ func p1(ctx context.Context) {
 	fmt.Printf("p1: %d\n", m.walk())
 }
 
+func p2(ctx context.Context) {
+	txt := input.NewTXTFile("10.txt")
+	g := grid.NewVecMatrix[uint8]()
+	txt.ReadByLineEx(ctx, func(i int, line string) error {
+		for j, c := range line {
+			g.Add(grid.Vec{
+				X: j,
+				Y: i,
+			}, byte(c))
+		}
+		return nil
+	})
+	m := maze{g}
+	fmt.Printf("p2: %d\n", m.walk2())
+}
+
 func main() {
 	ctx := context.Background()
 	p1(ctx)
+	p2(ctx)
 }
