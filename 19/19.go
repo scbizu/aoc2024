4849b942492d00b535dc4a88225a33756a224cb2
scnace <scbizu@gmail.com>
2024-12-20 02:25:29 +0800

    19: dp and dfs+cache

--- 19/19.go
+++ 19/19.go
@@ -3,80 +3,81 @@ package main
 import (
 	"context"
 	"fmt"
-	"slices"
 	"strings"
 
 	"github.com/magejiCoder/magejiAoc/input"
-	"github.com/magejiCoder/magejiAoc/queue"
 	"github.com/magejiCoder/magejiAoc/set"
 )
 
 type towel struct {
 	stripes *set.Set[string]
+	cache   map[string]int
 }
 
-type state struct {
-	read []string
-	left string
-}
+func (t *towel) combinations(pattern string) int {
+	// 这里虽然是全遍历，但是还是dfs + cache 好， 因为它可以在每次返回值的时候就开始构建cache，效率会高很多
+	// 而 bfs 只能对每个 pattern 都构建一次 cache，效率低很多
+	var dfs func(remaining string) int
+	dfs = func(remaining string) int {
+		// fmt.Printf("dfs:  remaining: %s\n", remaining)
+		if len(remaining) == 0 {
+			t.cache[pattern] += 1
+			return 1
+		}
 
-func (t towel) spilitPattern(pattern []byte) [][]string {
-	// fmt.Printf("pattern: %s\n", string(pattern))
-	q := queue.NewQueue[state]()
-	q.Push(state{
-		read: []string{string(pattern[:1])},
-		left: string(pattern[1:]),
-	})
-	var patterns [][]string
-	for q.Len() > 0 {
-		s := q.Pop()
-		fmt.Printf("pop: %s\n", s)
-		latest := s.read[len(s.read)-1]
-		if len(s.left) == 0 {
-			// handle the last byte
-			patterns = append(patterns, s.read)
+		if result, ok := t.cache[remaining]; ok {
+			return result
 		}
-		if len(s.left) > 0 {
-			fmt.Printf("next1: %s\n", s.left[:1])
-			if t.stripes.Has(string(s.left[:1])) {
-				// append 1 byte
-				q.Push(state{
-					read: append(slices.Clone(s.read), string(s.left[:1])),
-					left: string(s.left[1:]),
-				})
+		var total int
+		t.stripes.Each(func(stripe string) bool {
+			if strings.HasPrefix(remaining, stripe) {
+				count := dfs(remaining[len(stripe):])
+				if len(remaining[len(stripe):]) > 0 {
+					// fmt.Printf("set cache: key: %s, value: %d\n", remaining[len(stripe):], count)
+					t.cache[remaining[len(stripe):]] = count
+				}
+				total += count
 			}
-			fmt.Printf("next2: %s\n", latest+s.left[:1])
-			if t.stripes.Has(latest + s.left[:1]) {
-				// append to latest byte
-				q.Push(state{
-					read: append(slices.Clone(s.read[:len(s.read)-1]), latest+s.left[:1]),
-					left: string(s.left[1:]),
-				})
-			}
-		}
+			return true
+		})
+
+		return total
 	}
-	return patterns
+
+	return dfs(pattern)
 }
 
-func (t towel) isPatternValid(
-	pattern string,
-) bool {
-	for _, ps := range t.spilitPattern([]byte(pattern)) {
-		valid := true
-		// fmt.Printf("ps: %v\n", ps)
-		// every ps should present in stripes
-		for _, p := range ps {
-			if !t.stripes.Has(p) {
-				fmt.Printf("don't present in strips: %v\n", p)
-				valid = false
-				break
-			}
-		}
-		if valid {
+func (t *towel) checkPattern(pattern string) bool {
+	cache := make(map[string]bool)
+	// 这里要用 dfs , 不能用 bfs
+	// dfs + cache 的方式命中率要大大高于 bfs + cache 的方式
+	// 并且对应的 stripe 越多，dfs 的效果越明显 , 相比之下 bfs 的分支太多了，而且 cache 只在很上层生效, build 的效率太低了
+	var dfs func(remaining string) bool
+	dfs = func(remaining string) bool {
+		if len(remaining) == 0 {
 			return true
 		}
+
+		if result, ok := cache[remaining]; ok {
+			return result
+		}
+
+		result := false
+		t.stripes.Each(func(stripe string) bool {
+			if strings.HasPrefix(remaining, stripe) {
+				if dfs(remaining[len(stripe):]) {
+					result = true
+					return false
+				}
+			}
+			return true
+		})
+
+		cache[remaining] = result
+		return result
 	}
-	return false
+
+	return dfs(pattern)
 }
 
 func p1(ctx context.Context) {
@@ -97,15 +98,41 @@ func p1(ctx context.Context) {
 	})
 	var total int
 	for _, pattern := range patterns {
-		if t.isPatternValid(pattern) {
-			fmt.Printf("valid: %s\n", pattern)
+		if t.checkPattern(pattern) {
+			// fmt.Printf("valid: %s\n", pattern)
 			total++
 		}
 	}
 	fmt.Printf("p1: %d\n", total)
 }
 
+func p2(ctx context.Context) {
+	txt := input.NewTXTFile("19.txt")
+	t := &towel{
+		stripes: set.New[string](),
+		cache:   make(map[string]int),
+	}
+	var patterns []string
+	txt.ReadByBlock(ctx, "\n\n", func(block []string) error {
+		// block0 is the stripes
+		stripes := block[0]
+		for _, stripe := range strings.Split(stripes, ", ") {
+			t.stripes.Add(stripe)
+		}
+		// block1 is the patterns
+		patterns = append(patterns, strings.Split(block[1], "\n")...)
+		return nil
+	})
+	var total int
+	for _, pattern := range patterns {
+		total += t.combinations(pattern)
+	}
+	// fmt.Printf("cache: %v\n", t.cache)
+	fmt.Printf("p2: %d\n", total)
+}
+
 func main() {
 	ctx := context.Background()
 	p1(ctx)
+	p2(ctx)
 }
