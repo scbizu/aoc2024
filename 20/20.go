1b12207bb76e78ae669e66c6dbbe32e522c432ad
scnace <scbizu@gmail.com>
2024-12-21 02:11:45 +0800

    20: bfs

--- 20/20.go
+++ 20/20.go
@@ -3,10 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
-	"slices"
 
 	"github.com/magejiCoder/magejiAoc/grid"
 	"github.com/magejiCoder/magejiAoc/input"
+	"github.com/magejiCoder/magejiAoc/math"
 	"github.com/magejiCoder/magejiAoc/queue"
 	"github.com/magejiCoder/magejiAoc/set"
 )
@@ -29,13 +29,14 @@ type raceTrack struct {
 	m grid.VecMatrix[byte]
 
 	start, end grid.Vec
-	wall       grid.Vec
+	lastTrace  *set.Set[grid.Vec]
+	cache      map[grid.Vec]int
 }
 
 type state struct {
-	v    grid.Vec
-	d    direction
-	path *set.Set[grid.Vec]
+	v          grid.Vec
+	path       *set.Set[grid.Vec]
+	foundOther bool
 }
 
 func copyMaze(m grid.VecMatrix[byte]) grid.VecMatrix[byte] {
@@ -48,178 +49,186 @@ func copyMaze(m grid.VecMatrix[byte]) grid.VecMatrix[byte] {
 
 func (r raceTrack) clone() raceTrack {
 	return raceTrack{
-		m:     copyMaze(r.m),
-		start: r.start,
-		end:   r.end,
+		m:         copyMaze(r.m),
+		start:     r.start,
+		end:       r.end,
+		lastTrace: r.lastTrace,
 	}
 }
 
-func (r *raceTrack) removeInnerWall(ctx context.Context, base int) map[int]int {
+func isNear(v1, v2 grid.Vec) bool {
+	v := v1.Sub(v2)
+	return math.Abs(v.X)+math.Abs(v.Y) == 1
+}
+
+func (r *raceTrack) removeInnerWall(ctx context.Context) map[int]int {
+	base := r.lastTrace.Size()
 	// find wall
 	innerWalls := set.New[grid.Vec]()
 	r.m.ForEach(func(v grid.Vec, b byte) {
-		if v.X == 0 || v.Y == 0 || v.X == r.m.Cols()-1 || v.Y == r.m.Rows()-1 || v == r.wall {
+		if b != '#' {
 			return
 		}
-		if b == '#' {
-			innerWalls.Add(v)
-		}
+		r.lastTrace.Each(func(item grid.Vec) bool {
+			if isNear(v, item) {
+				fmt.Printf("item,v: %v,%v\n", item, v)
+				innerWalls.Add(v)
+			}
+			return true
+		})
 	})
+	fmt.Printf("#: %d\n", innerWalls.Size())
 	paths := make(map[int]int)
 	innerWalls.Each(func(item grid.Vec) bool {
 		r2 := r.clone()
 		r2.m.Add(item, '.')
-		_, p := r2.walk(ctx)
-		fmt.Printf("rm wall: %v,cost: %d\n", item, p)
-		if p != -1 && base-p > 0 {
-			paths[base-p]++
+		p := r2.walk(ctx)
+		// fmt.Printf("rm wall: %v,cost: %d\n", item, p.Size())
+		if p.Size() > 0 && base-p.Size() > 0 {
+			paths[base-p.Size()]++
 		}
 		return true
 	})
 	return paths
 }
 
-func (r raceTrack) walk2(ctx context.Context) int {
-	visited := make(map[dirVec]int)
-	dirs := []direction{up, down, left, right}
-	var dfs func(v grid.Vec, d direction, path *set.Set[grid.Vec]) int
-	dfs = func(v grid.Vec, d direction, path *set.Set[grid.Vec]) int {
-		if v == r.end {
-			return path.Size()
-		}
-		var nextV grid.Vec
-		switch d {
-		case up:
-			nextV = v.Add(grid.Vec{X: 0, Y: -1})
-		case down:
-			nextV = v.Add(grid.Vec{X: 0, Y: 1})
-		case left:
-			nextV = v.Add(grid.Vec{X: -1, Y: 0})
-		case right:
-			nextV = v.Add(grid.Vec{X: 1, Y: 0})
-		}
-		c, ok := r.m.Get(nextV)
-		if !ok {
-			// wall
-			return -1
-		}
-		if c == '#' {
-			// wall
-			return -1
-		}
-		// fmt.Printf("v: %v,d: %d,path: %v,trace: %d\n", nextV, d, path.List(), path.Size())
-		if v, ok := visited[dirVec{
-			v: nextV,
-			d: d,
-		}]; ok && v <= path.Size()+1 {
-			return -1
-		}
-		visited[dirVec{
-			v: nextV,
-			d: d,
-		}] = path.Size() + 1
-		// fmt.Printf("visited: %v\n", visited)
-		p := path.Copy()
-		p.Add(nextV)
-		var totals []int
-		fmt.Printf("visit: %v, trace: %v\n", nextV, p)
-		for _, d := range dirs {
-			t := dfs(nextV, d, p)
-			if t != -1 {
-				totals = append(totals, t)
-			}
-		}
-		fmt.Printf("totals: %v\n", totals)
-		if len(totals) == 0 {
-			return -1
-		}
-		slices.Sort(totals)
-		return totals[0]
+//	func (r raceTrack) walk2(ctx context.Context) *set.Set[grid.Vec] {
+//		dirs := []direction{up, down, left, right}
+//		var dfs func(v grid.Vec, d direction, path *set.Set[grid.Vec])
+//		dfs = func(v grid.Vec, d direction, path *set.Set[grid.Vec])  {
+//			if v == r.end {
+//				return
+//			}
+//			var nextV grid.Vec
+//			switch d {
+//			case up:
+//				nextV = v.Add(grid.Vec{X: 0, Y: -1})
+//			case down:
+//				nextV = v.Add(grid.Vec{X: 0, Y: 1})
+//			case left:
+//				nextV = v.Add(grid.Vec{X: -1, Y: 0})
+//			case right:
+//				nextV = v.Add(grid.Vec{X: 1, Y: 0})
+//			}
+//			if path.Has(nextV) {
+//				return
+//			}
+//			c, ok := r.m.Get(nextV)
+//			if !ok {
+//				// wall
+//				return
+//			}
+//			if c == '#' {
+//				// wall
+//				return
+//			}
+//			// fmt.Printf("visited: %v\n", visited)
+//			p := path.Copy()
+//			p.Add(nextV)
+//			var totals []int
+//			for _, d := range dirs {
+//				t := dfs(nextV, d, p)
+//				if t != -1 {
+//					totals = append(totals, t)
+//					break
+//				}
+//			}
+//			fmt.Printf("totals: %v\n", totals)
+//			if len(totals) == 0 {
+//				return -1
+//			}
+//			slices.Sort(totals)
+//			return totals[0]
+//		}
+//		for _, d := range dirs {
+//			next := dfs(r.start, d, set.New[grid.Vec]())
+//			if next != -1 {
+//				return next
+//			}
+//		}
+//		return -1
+//	}
+func addVec(v grid.Vec, d direction) grid.Vec {
+	switch d {
+	case up:
+		return v.Add(grid.Vec{X: 0, Y: -1})
+	case down:
+		return v.Add(grid.Vec{X: 0, Y: 1})
+	case left:
+		return v.Add(grid.Vec{X: -1, Y: 0})
+	case right:
+		return v.Add(grid.Vec{X: 1, Y: 0})
 	}
-	var t int
-	for _, d := range dirs {
-		visited[dirVec{
-			v: r.start,
-			d: d,
-		}] = 0
-		t += dfs(r.start, d, set.New[grid.Vec]())
-	}
-	return t
+	panic("invalid direction")
 }
 
-func (r raceTrack) walk(_ context.Context) (grid.Vec, int) {
-	// visited := make(map[grid.Vec]int)
+func (r raceTrack) walk(_ context.Context) *set.Set[grid.Vec] {
 	q := queue.NewQueue[state]()
 	dirs := []direction{up, down, left, right}
-	var lastV grid.Vec
-	for _, d := range dirs {
-		q.Push(state{
-			v:    r.start,
-			d:    d,
-			path: set.New[grid.Vec](),
-		})
-		// visited[r.start] = 0
-	}
-	var totals []int
+	q.Push(state{
+		v: r.start,
+		path: set.New[grid.Vec](
+			r.start,
+		),
+	})
 	for q.Len() > 0 {
 		st := q.Pop()
 		if st.v == r.end {
-			totals = append(totals, st.path.Size())
-			lastV = st.path.List()[st.path.Size()-1]
-			break
+			st.path.Remove(r.end)
+			return st.path
 		}
-		var nextV grid.Vec
-		switch st.d {
-		case up:
-			nextV = st.v.Add(grid.Vec{X: 0, Y: -1})
-		case down:
-			nextV = st.v.Add(grid.Vec{X: 0, Y: 1})
-		case left:
-			nextV = st.v.Add(grid.Vec{X: -1, Y: 0})
-		case right:
-			nextV = st.v.Add(grid.Vec{X: 1, Y: 0})
-		}
-		c, ok := r.m.Get(nextV)
-		if !ok {
-			// wall
-			continue
-		}
-		if c == '#' {
-			// wall
-			continue
-		}
-		if st.path.Has(nextV) {
-			continue
-		}
-		// fmt.Printf("next: %v\n", nextV)
+		var validVecs []grid.Vec
 		for _, d := range dirs {
-			// vc, ok := visited[nextV]
-			// if ok && vc <= st.path.Size()+1 {
-			// 	// visited
-			// 	continue
-			// }
-			// visited[nextV] = st.path.Size() + 1
-			p := st.path.Copy()
-			p.Add(st.v)
-			// fmt.Printf("push: %v,path: %v\n", nextV, p.List())
-			q.Push(state{
-				v:    nextV,
-				d:    d,
-				path: p,
-			})
+			vc := addVec(st.v, d)
+			c, ok := r.m.Get(vc)
+			if !ok {
+				// wall
+				continue
+			}
+			if c == '#' {
+				// wall
+				continue
+			}
+			if st.path.Has(vc) {
+				continue
+			}
+			validVecs = append(validVecs, vc)
 		}
-
+		switch len(validVecs) {
+		case 1:
+			path := st.path.Copy()
+			path.Add(validVecs[0])
+			q.Push(state{
+				v:          validVecs[0],
+				path:       path,
+				foundOther: st.foundOther,
+			})
+		case 2:
+			for _, vv := range validVecs {
+				if r.lastTrace.Has(vv) && !st.foundOther {
+					continue
+				}
+				// choose the other way
+				path := st.path.Copy()
+				path.Add(vv)
+				q.Push(state{
+					v:          vv,
+					path:       path,
+					foundOther: true,
+				})
+			}
+		case 3, 4:
+			panic("bad route")
+		}
 	}
-	if len(totals) == 0 {
-		return grid.Vec{}, -1
-	}
-	return lastV, totals[0]
+	return set.New[grid.Vec]()
 }
 
 func p1(ctx context.Context) {
 	txt := input.NewTXTFile("20.txt")
 	rs := &raceTrack{
-		m: grid.NewVecMatrix[byte](),
+		m:         grid.NewVecMatrix[byte](),
+		lastTrace: set.New[grid.Vec](),
 	}
 	txt.ReadByLineEx(ctx, func(i int, line string) error {
 		for j, c := range line {
@@ -242,10 +251,10 @@ func p1(ctx context.Context) {
 		}
 		return nil
 	})
-	last, base := rs.walk(ctx)
-	fmt.Printf("base: %d\n", base)
-	rs.wall = last
-	deltas := rs.removeInnerWall(ctx, base)
+	base := rs.walk(ctx)
+	fmt.Printf("base: %d\n", base.Size())
+	rs.lastTrace = base
+	deltas := rs.removeInnerWall(ctx)
 	least := 100
 	var total int
 	for k, v := range deltas {
