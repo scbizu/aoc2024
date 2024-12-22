299a56828c683e489802df881a0626541742ca6a
scnace <scbizu@gmail.com>
2024-12-23 02:23:55 +0800

    21: 2 bfs , one for searing , one for build string

--- 21/21.go
+++ 21/21.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"fmt"
+	"math"
+	"slices"
 
 	"github.com/magejiCoder/magejiAoc/input"
 	"github.com/magejiCoder/magejiAoc/queue"
@@ -64,8 +65,8 @@ var numPad = map[byte][]numDir{
 	'A': {{num: '0', dir: left}, {num: '3', dir: up}},
 	'0': {{num: 'A', dir: right}, {num: '2', dir: up}},
 	'1': {{num: '2', dir: right}, {num: '4', dir: up}},
-	'2': {{num: '1', dir: left}, {num: '5', dir: up}, {num: '0', dir: down}},
-	'3': {{num: 'A', dir: down}, {num: '6', dir: up}, {num: '1', dir: left}},
+	'2': {{num: '1', dir: left}, {num: '5', dir: up}, {num: '0', dir: down}, {num: '3', dir: right}},
+	'3': {{num: 'A', dir: down}, {num: '6', dir: up}, {num: '2', dir: left}},
 	'4': {{num: '1', dir: down}, {num: '7', dir: up}, {num: '5', dir: right}},
 	'5': {{num: '2', dir: down}, {num: '8', dir: up}, {num: '4', dir: left}, {num: '6', dir: right}},
 	'6': {{num: '3', dir: down}, {num: '9', dir: up}, {num: '5', dir: left}},
@@ -83,15 +84,16 @@ var dirPad = map[byte][]numDir{
 }
 
 func (c *ctrl1) apply(b byte) pathTrack {
-	// fmt.Printf("current: %c, target: %c\n", c.current, b)
-
 	q := queue.NewQueue[state]()
 	q.Push(state{current: c.current, path: []direction{}, visited: set.New(c.current)})
 	var ret pathTrack
 
 	for q.Len() > 0 {
 		p := q.Pop()
+		// fmt.Printf("current: %c, path: %v\n", p.current, p.path)
 		if p.current == b {
+			// fmt.Printf("dir: %v\n", dirsToString(p.path))
+			c.current = b
 			ret = append(ret, dirsToString(p.path))
 		}
 		dirs := p.path
@@ -99,9 +101,11 @@ func (c *ctrl1) apply(b byte) pathTrack {
 			if p.visited.Has(next.num) {
 				continue
 			}
+			// fmt.Printf("next: %c,dir: %v\n", next.num, next.dir)
 			pc := p.visited.Copy()
 			pc.Add(next.num)
-			q.Push(state{current: next.num, path: append(dirs, next.dir), visited: pc})
+			np := slices.Clone(dirs)
+			q.Push(state{current: next.num, path: append(np, next.dir), visited: pc})
 		}
 	}
 	return ret
@@ -109,10 +113,16 @@ func (c *ctrl1) apply(b byte) pathTrack {
 
 // ctrl2 is the robot controller .
 type ctrl2 struct {
-	current byte
+	current   byte
+	ctrlCache map[target]pathTrack
 }
 
-func (c2 *ctrl2) apply(fb byte) pathTrack {
+type target struct {
+	from byte
+	to   byte
+}
+
+func (c2 *ctrl2) apply(fb byte, last bool) pathTrack {
 	var ret pathTrack
 	q := queue.NewQueue[state]()
 	q.Push(state{current: c2.current, path: []direction{}, visited: set.New(c2.current)})
@@ -120,6 +130,10 @@ func (c2 *ctrl2) apply(fb byte) pathTrack {
 		p := q.Pop()
 		if p.current == fb {
 			ret = append(ret, dirsToString(p.path))
+			c2.current = fb
+			if last {
+				break
+			}
 		}
 		dirs := p.path
 		for _, next := range dirPad[p.current] {
@@ -128,7 +142,8 @@ func (c2 *ctrl2) apply(fb byte) pathTrack {
 			}
 			pc := p.visited.Copy()
 			pc.Add(next.num)
-			q.Push(state{current: next.num, path: append(dirs, next.dir), visited: pc})
+			pn := slices.Clone(dirs)
+			q.Push(state{current: next.num, path: append(pn, next.dir), visited: pc})
 		}
 	}
 	return ret
@@ -144,50 +159,206 @@ func toInt(seq string) int {
 	return input.Atoi(intPart)
 }
 
+type item struct {
+	index int
+	path  []string
+}
+
+func buildDigitalPad(seq []byte) []string {
+	var ret []string
+	q := queue.NewQueue[item]()
+	q.Push(item{index: 0, path: []string{}})
+	c1 := &ctrl1{current: 'A'}
+	for q.Len() > 0 {
+		it := q.Pop()
+		if it.index >= len(seq) {
+			ret = it.path
+			break
+		}
+		paths := c1.apply(seq[it.index])
+		var nps []string
+		if len(it.path) == 0 {
+			nps = paths
+		} else {
+			for _, p := range it.path {
+				for _, path := range paths {
+					nps = append(nps, p+path)
+				}
+			}
+		}
+		// fmt.Printf("nps: %v\n", paths)
+		q.Push(item{index: it.index + 1, path: nps})
+	}
+	// filter out the shortest ret
+	minLen := len(ret[0])
+	for _, r := range ret {
+		if len(r) < minLen {
+			minLen = len(r)
+		}
+	}
+	var nret []string
+	for _, r := range ret {
+		if len(r) == minLen {
+			nret = append(nret, r)
+		}
+	}
+	return nret
+}
+
+func buildRobotPad(seq []byte, last bool) []string {
+	var ret []string
+	q := queue.NewQueue[item]()
+	q.Push(item{index: 0, path: []string{}})
+	c2 := &ctrl2{current: 'A'}
+	for q.Len() > 0 {
+		it := q.Pop()
+		if it.index >= len(seq) {
+			ret = it.path
+			break
+		}
+		paths := c2.apply(seq[it.index], last)
+		var nps []string
+		if len(it.path) == 0 {
+			nps = paths
+		} else {
+			for _, p := range it.path {
+				for _, path := range paths {
+					nps = append(nps, p+path)
+				}
+			}
+		}
+		q.Push(item{index: it.index + 1, path: nps})
+	}
+	// filter out the shortest ret
+	minLen := len(ret[0])
+	for _, r := range ret {
+		if len(r) < minLen {
+			minLen = len(r)
+		}
+	}
+	var nret []string
+	for _, r := range ret {
+		if len(r) == minLen {
+			nret = append(nret, r)
+		}
+	}
+	return nret
+}
+
+func filterShortest(ret []string) []string {
+	minLen := len(ret[0])
+	for _, r := range ret {
+		if len(r) < minLen {
+			minLen = len(r)
+		}
+	}
+	var nret []string
+	for _, r := range ret {
+		if len(r) == minLen {
+			nret = append(nret, r)
+		}
+	}
+	return nret
+}
+
 func p1(ctx context.Context) {
 	txt := input.NewTXTFile("21.txt")
 	var seqs []string
 	txt.ReadByLineEx(ctx, func(_ int, line string) error {
+		if len(line) == 0 {
+			return nil
+		}
 		seqs = append(seqs, line)
 		return nil
 	})
-	var outs []string
-	for _, seq := range seqs {
-		c1 := &ctrl1{current: 'A'}
-		bf := bytes.NewBuffer(nil)
-		for _, b := range []byte(seq) {
-			dirs := c1.apply(b)
-			for _, d := range dirs {
-				bf.WriteString(d.String())
-			}
-			bf.WriteString("A")
-		}
-		fmt.Printf("c1: %s\n", bf.String())
-		in := bf.Bytes()
-		for i := 0; i < 2; i++ {
-			c2 := &ctrl2{current: 'A'}
-			c2b := bytes.NewBuffer(nil)
-			for _, b := range in {
-				dirs := c2.apply(b)
-				for _, d := range dirs {
-					c2b.WriteString(d.String())
-				}
-				c2b.WriteString("A")
-			}
-			in = c2b.Bytes()
-			fmt.Printf("c2(%d): %s\n", i, c2b.String())
-		}
-		outs = append(outs, string(in))
-	}
+	fmt.Printf("seqs: %v\n", seqs)
 	var sum int
-	for i, out := range outs {
-		fmt.Printf("seq: %d, out: %d\n", toInt(seqs[i]), len(out))
-		sum += len(out) * toInt(seqs[i])
+	for _, seq := range seqs {
+		next := buildDigitalPad([]byte(seq))
+		// fmt.Printf("next: %v\n", next)
+		for i := 0; i < 2; i++ {
+			shortest := math.MaxInt32
+			var nx []string
+			for _, n := range next {
+				// if i == 1 { fmt.Printf("n: %v\n", n)
+				// }
+				r2 := buildRobotPad([]byte(n), i == 1)
+				if len(filterShortest(r2)[0]) <= shortest {
+					shortest = len(filterShortest(r2)[0])
+					nx = append(nx, filterShortest(r2)...)
+				}
+			}
+			next = nx
+			// fmt.Printf("next r: %v\n", next)
+		}
+		minLen := len(next[0])
+		for _, r := range next {
+			if len(r) < minLen {
+				minLen = len(r)
+			}
+		}
+		var nret []string
+		for _, r := range next {
+			if len(r) == minLen {
+				nret = append(nret, r)
+			}
+		}
+		// fmt.Printf("n: %v\n", len(nret[0]))
+		sum += toInt(seq) * len(nret[0])
 	}
 	fmt.Printf("p1: %d\n", sum)
+}
+
+func p2(ctx context.Context) {
+	txt := input.NewTXTFile("21.txt")
+	var seqs []string
+	txt.ReadByLineEx(ctx, func(_ int, line string) error {
+		if len(line) == 0 {
+			return nil
+		}
+		seqs = append(seqs, line)
+		return nil
+	})
+	// fmt.Printf("seqs: %v\n", seqs)
+	var sum int
+	for _, seq := range seqs {
+		next := buildDigitalPad([]byte(seq))
+		// fmt.Printf("next: %v\n", next)
+		for i := 0; i < 25; i++ {
+			shortest := math.MaxInt32
+			var nx []string
+			for _, n := range next {
+				// if i == 1 { fmt.Printf("n: %v\n", n)
+				// }
+				r2 := buildRobotPad([]byte(n), i == 24)
+				if len(filterShortest(r2)[0]) <= shortest {
+					shortest = len(filterShortest(r2)[0])
+					nx = append(nx, filterShortest(r2)...)
+				}
+			}
+			next = nx
+			// fmt.Printf("next r: %v\n", next)
+		}
+		minLen := len(next[0])
+		for _, r := range next {
+			if len(r) < minLen {
+				minLen = len(r)
+			}
+		}
+		var nret []string
+		for _, r := range next {
+			if len(r) == minLen {
+				nret = append(nret, r)
+			}
+		}
+		// fmt.Printf("n: %v\n", len(nret[0]))
+		sum += toInt(seq) * len(nret[0])
+	}
+	fmt.Printf("p2: %d\n", sum)
 }
 
 func main() {
 	ctx := context.Background()
 	p1(ctx)
+	// p2(ctx)
 }
