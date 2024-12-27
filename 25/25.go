49324564474f11f3153b823c5fdb305b89493b55
scbizu <scbizu@gmail.com>
2024-12-27 14:27:19 +0800

    25: happy AoC

--- 25/25.go
+++ 25/25.go
@@ -17,8 +17,8 @@ func (k key) toPattern() []int {
 	var rows []int
 	for col := 0; col < k.m.Cols(); col++ {
 		var c int
-		for row := k.m.Rows() - 1; row > 0; row-- {
-			if k.m[grid.Vec{X: col, Y: row}] == '#' {
+		for row := k.m.Rows() - 2; row >= 0; row-- {
+			if k.m[grid.Vec{X: col, Y: row}] != '#' {
 				continue
 			}
 			c++
@@ -36,8 +36,8 @@ func (l lock) toPattern() []int {
 	var rows []int
 	for col := 0; col < l.m.Cols(); col++ {
 		var c int
-		for row := 0; row < l.m.Rows(); row++ {
-			if l.m[grid.Vec{X: col, Y: row}] == '#' {
+		for row := 1; row < l.m.Rows(); row++ {
+			if l.m[grid.Vec{X: col, Y: row}] != '#' {
 				continue
 			}
 			c++
@@ -68,14 +68,27 @@ func p1(ctx context.Context) {
 		}
 		return nil
 	})
+	// fmt.Printf("keys: %d;locks: %d\n", len(keys), len(locks))
 	col, row := keys[0].m.Cols(), keys[0].m.Rows()
+	// fmt.Printf("cols: %d,rows: %d\n", col, row)
 	var fit int
 	for _, k := range keys {
 		for _, l := range locks {
+			var isFit bool
+			// fmt.Printf("[key:%d] pattern: %v\n", ki, k.toPattern())
+			// fmt.Printf("[lock:%d] pattern: %v\n", li, l.toPattern())
 			for i := 0; i < col; i++ {
-				if k.toPattern()[i]+l.toPattern()[i] <= row {
-					fit++
+				// fmt.Printf("[%d:%d]num: %d to %d\n", ki, li, k.toPattern()[i], l.toPattern()[i])
+				if k.toPattern()[i]+l.toPattern()[i] <= row-2 {
+					isFit = true
+				} else {
+					isFit = false
+					break
 				}
+			}
+			if isFit {
+				// fmt.Printf("keys: %v\n; locks: %v\n", k, l)
+				fit++
 			}
 		}
 	}
